8aa42b3626b39aadfb3729e93ce5f3c1f37399cb
kymx1983 <kymx1983@gmail.com>
2023-11-07 21:44:38 +0900

    add new create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,26 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+  private
+    # Strong Parametersを使用して安全にパラメータをホワイトリストに登録
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: [:index]
+  # resources :tasks, only: [:index]
+  resources :tasks
   # Defines the root path route ("/")
   # root "articles#index"
 end
